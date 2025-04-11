b3624ef516c08a61c79c2bfd20229bc02ca7c889
Redream <mr.golyadkina@mail.ru>
2025-04-12 00:15:28 +0300

    feat: implement character flip

--- Assets/_Project/Logic/Scripts/Character/Character.cs
+++ Assets/_Project/Logic/Scripts/Character/Character.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,11 +9,20 @@ public class Character : MonoBehaviour, IControllable
     private Vector2 _moveDirection;
     private Vector2 _moveVelocity;
 
+    private bool _isFacingRight;
+
     private Rigidbody2D _rb;
 
     private void Awake()
     {
+        _isFacingRight = true;
+
         _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        FlipCheck();
     }
 
     private void FixedUpdate()
@@ -27,7 +37,8 @@ public class Character : MonoBehaviour, IControllable
 
     private void MoveInternal()
     {
-        if(_moveDirection != Vector2.zero)
+       
+        if (_moveDirection != Vector2.zero)
         {
             var targetVelocity = Vector2.zero;
             targetVelocity = _moveDirection * _playerConfig.speed;
@@ -39,6 +50,33 @@ public class Character : MonoBehaviour, IControllable
         {
             _moveVelocity = Vector2.Lerp(_moveVelocity, Vector2.zero, _playerConfig.deceleration * Time.fixedDeltaTime);
             _rb.linearVelocity = new Vector2(_moveVelocity.x, _rb.linearVelocity.y);
+        }
+    }
+
+    private void FlipCheck()
+    {
+        if (_isFacingRight && _moveDirection.x < 0)
+        {
+            Flip(false);
+        }
+
+        else if (!_isFacingRight && _moveDirection.x > 0)
+        {
+            Flip(true);
+        }
+    }
+
+    private void Flip(bool flipRight)
+    {
+        if (flipRight)
+        {
+            _isFacingRight = true;
+            transform.Rotate(0, 180f, 0);
+        }
+        else
+        {
+            _isFacingRight = false;
+            transform.Rotate(0, -180f, 0);
         }
     }
 }
